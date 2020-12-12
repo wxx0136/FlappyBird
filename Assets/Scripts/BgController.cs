ea71479a9a797bb46ea598f515d22393947a3b5f
wxx <wxx0136@gmail.com>
2020-12-11 20:16:00 -0600

    Add bird animator

---
+++ Assets/Scripts/BgController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgController : MonoBehaviour
+{
+    Vector3 start_postion;
+    public float speed = -0.02f;
+    public float set_off = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        start_postion = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -11.4f + set_off)
+        {
+            transform.position = start_postion;
+        }
+        transform.Translate(speed, 0, 0);
+    }
+}
---
+++ Assets/Scripts/BirdFly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdFly : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.SetInteger("state", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb2d.velocity = new Vector2(0, 8);
+        }
+    }
+}
