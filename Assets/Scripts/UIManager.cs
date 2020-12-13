1de1684180cf31dfcfc7dcac1d07bda39dcd13d6
wxx <wxx0136@gmail.com>
2020-12-12 23:07:30 -0600

    Add DoTween plug-in

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public void ShowUI()
+    {
+        canvasGroup.alpha = 1f;
+    }
+
+    public void HideUI()
+    {
+        canvasGroup.DOFade()
+    }
+}
