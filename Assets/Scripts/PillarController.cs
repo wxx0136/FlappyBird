abd28a13d3317842123b2c56e90812973f2fb593
wxx <wxx0136@gmail.com>
2020-12-13 20:22:11 -0600

    Create Pipes

---
+++ Assets/Scripts/PillarController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarController : MonoBehaviour
+{
+    public Transform pipes;
+    public GameObject pipePrefab;
+
+    public void StartMove()
+    {
+    }
+
+    public void StopMOve()
+    {
+    }
+
+    public void SpawnOnePipe()
+    {
+        var newPipe = Instantiate(pipePrefab, pipes);
+        newPipe.GetComponent<PipeScript>().RandomHeightOfPipes();
+    }
+}
---
+++ Assets/Scripts/PipeScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PipeScript : MonoBehaviour
+{
+    public float speed = 0.05f;
+    public bool canMove = true;
+
+    public void FixedUpdate()
+    {
+        if (!canMove) return;
+
+        transform.Translate(-speed, 0, 0);
+    }
+
+    public void RandomHeightOfPipes()
+    {
+        var ran = Random.Range(-1.8f, 2.5f);
+        transform.SetPositionAndRotation(new Vector3(transform.position.x, ran, transform.position.z),
+            transform.rotation);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            RandomHeightOfPipes();
+        }
+    }
+}
